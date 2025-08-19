4c2587ef5184e36b3eacf5ac72d55ae21e3723f2
vibhu <vibhu63205@gmail.com>
2025-08-19 17:23:40 +0100

    passed thanks...!

--- app/views.py
+++ app/views.py
@@ -27,7 +27,9 @@ class ProductDetailView(View):  # ✅ fixed casing
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
         item_already_in_cart = False
-        item_already_in_cart = Cart.objects.filter(Q(product=product.id) & Q(user=request.user)).exists()
+        if request.user.is_authenticated:
+
+         item_already_in_cart = Cart.objects.filter(Q(product=product.id) & Q(user=request.user)).exists()
         return render(request, 'app/productdetail.html', {'product': product,'item_already_in_cart': item_already_in_cart})
 
 @login_required
